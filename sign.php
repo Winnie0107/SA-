4129f4d5e01204474341a524f7b373c20d79674e
hpxx422 <166095574+hpxx422@users.noreply.github.com>
2024-04-20 16:33:01 +0800

    signin

---
+++ sign.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $link = mysqli_connect('localhost:3307', 'root', '12345678', 'box');
+
+    $account = isset($_POST['account']) ? mysqli_real_escape_string($link, $_POST['account']) : '';
+    $password = isset($_POST['password']) ? mysqli_real_escape_string($link, $_POST['password']) : '';
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($link, $_POST['email']) : '';
+    $level = isset($_POST['level']) ? mysqli_real_escape_string($link, $_POST['level']) : '';
+
+    $sql = "INSERT INTO user (account, password, email, level) VALUES ('$account', '$password', '$email', '$level')";
+
+    if (mysqli_query($link, $sql)) {
+        header("Location: http://localhost/SA-/login.html");
+        exit();
+    } 
+    else {
+        echo "錯誤: " . $sql . "<br>" . mysqli_error($link);
+    }
+
+    mysqli_close($link);
+}
+?>
