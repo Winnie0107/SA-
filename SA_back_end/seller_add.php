43cd24a44f9f4547007539eb57c0bcec0a02fefe
Winnie <winniehuang9217@gmail.com>
2024-05-06 17:04:00 +0800

    盲盒照片

--- SA_back_end/seller_add.php
+++ SA_back_end/seller_add.php
@@ -16,13 +16,31 @@ if (isset($_POST['submit'])) {
     $details = isset($_POST['details']) ? mysqli_real_escape_string($link, $_POST['details']) : '';
     $quantity = isset($_POST['quantity']) ? mysqli_real_escape_string($link, $_POST['quantity']) : '';
 
-    $form_data = isset($_FILES['img']['tmp_name']) ? $_FILES['img']['tmp_name'] : '';
+    // Set default image path based on price range
+    // 使用正則表達式檢查價格格式
+    if (preg_match('/^\$\d+$/', $price)) {
+        $price_value = intval(substr($price, 1)); // 提取價格數字部分並轉換為整數
+        if ($price_value > 0 && $price_value <= 500) {
+            $default_img_path = 'C:\AppServ\www\SA-\images\500.jpg';
+        } elseif ($price_value > 500 && $price_value <= 1000) {
+            $default_img_path = 'C:\AppServ\www\SA-\images\1000.jpg';
+        } elseif ($price_value > 1000 && $price_value <= 1500) {
+            $default_img_path = 'C:\AppServ\www\SA-\images\1500.jpg';
+        } else {
+            // 如果價格超出了範圍，設置一個默認的圖片路徑
+            $default_img_path = 'C:\AppServ\www\SA-\images\best.jpg';
+        }
+    } else {
+        // 如果價格格式不符合要求，設置一個默認的圖片路徑
+        $default_img_path = 'C:\AppServ\www\SA-\images\error.jpg';
+    }
 
-    $data = addslashes(file_get_contents($form_data));
+
+    // Read default image file and convert to binary format
+    $default_img_content = addslashes(file_get_contents($default_img_path));
 
     $result = mysqli_query($link, "INSERT INTO product (seller_ID, PName, category, price, details, quantity, img) 
-        VALUES ('$user_id', '$PName', '$category', '$price', '$details', '$quantity', '$data')");
-
+        VALUES ('$user_id', '$PName', '$category', '$price', '$details', '$quantity', '$default_img_content')");
 
     if ($result) {
         echo "<script>window.location.href = 'http://localhost/SA-/SA_front_end/seller_confirmation.php';</script>";
